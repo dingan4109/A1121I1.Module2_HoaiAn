2a6d47b26176b53c2cfdbee8d7ffbf2d3f87685e
dingan4109 <94068582+dingan4109@users.noreply.github.com>
2022-02-19 23:08:32 +0700

    bai tap

---
+++ src/ss1/ss1_javaIntro_baitap/SayHello.java
@@ -0,0 +1,14 @@
+package ss1.ss1_javaIntro_baitap;
+
+import java.util.Scanner;
+
+public class SayHello {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.printf("Hello %s",name);
+    }
+}
