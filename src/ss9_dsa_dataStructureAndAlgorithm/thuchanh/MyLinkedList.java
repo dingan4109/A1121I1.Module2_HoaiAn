5da7b517f651717e77f47502855e4c8e44f2564a
dingan4109 <94068582+dingan4109@users.noreply.github.com>
2022-03-13 12:03:32 +0700

    Thuc hanh

--- src/ss9_dsa_dataStructureAndAlgorithm/thuchanh/MyLinkedList.java
+++ src/ss9_dsa_dataStructureAndAlgorithm/thuchanh/MyLinkedList.java
@@ -3,7 +3,7 @@ package ss9_dsa_dataStructureAndAlgorithm.thuchanh;
 public class MyLinkedList {
 
     private Node head;
-    private int numNodes = 0;
+    private int numNodes; //size init = 0
 
     private class Node {
         private Node next;
@@ -18,11 +18,49 @@ public class MyLinkedList {
         }
     }
 
-
     public MyLinkedList(Object data) {
         head = new Node(data);
+        //numNodes++;
     }
 
-    
+    public void add(int index, Object data) {
+        Node preNode = null; //= holder
+        Node currentNode = head;
+        Node newNode = new Node(data);
+        for(int i = 1; i <= index;i++) {
+            preNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        newNode.next = currentNode;
+        preNode.next = newNode;
 
+        numNodes++;
+    }
+
+    public void addFirst(Object data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+
+        numNodes++;
+    }
+
+    public Node get(int index) {
+        Node current = head;
+        for(int i = 0; i<index;i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    public void printList() {
+        for(int i = 0; i<=numNodes;i++) {
+            System.out.print(head.data + "\t");
+            head = head.next;
+        }
+    }
+
+    public int getNumNodes() {
+        return numNodes;
+    }
 }
---
+++ src/ss9_dsa_dataStructureAndAlgorithm/thuchanh/MyLinkedListTest.java
@@ -0,0 +1,22 @@
+package ss9_dsa_dataStructureAndAlgorithm.thuchanh;
+
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        System.out.println(ll.getNumNodes());
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+        ll.addFirst(14);
+        ll.addFirst(15);
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.get(8);
+        System.out.println(ll.getNumNodes());
+           ll.printList();
+
+
+    }
+}
